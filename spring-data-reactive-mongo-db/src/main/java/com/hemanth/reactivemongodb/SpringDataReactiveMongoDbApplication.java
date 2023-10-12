73b396563ef95d0525d488efa504f86178a5c214
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-12 19:06:54 +0530

    1. Crud operations using mongo DB in reactive programming 2. Using java 8 features

---
+++ spring-data-reactive-mongo-db/src/main/java/com/hemanth/reactivemongodb/SpringDataReactiveMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.hemanth.reactivemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataReactiveMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataReactiveMongoDbApplication.class, args);
+    }
+
+}
