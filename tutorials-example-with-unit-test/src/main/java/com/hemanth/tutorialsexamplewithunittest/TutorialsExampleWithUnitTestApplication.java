e9a91dad870142f20534ab35768bf91b72bbcca2
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-13 20:13:52 +0530

    tutorials example with unit test cases

---
+++ tutorials-example-with-unit-test/src/main/java/com/hemanth/tutorialsexamplewithunittest/TutorialsExampleWithUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.hemanth.tutorialsexamplewithunittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialsExampleWithUnitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialsExampleWithUnitTestApplication.class, args);
+    }
+
+}
---
+++ tutorials-example-with-unit-test/src/test/java/com/hemanth/tutorialsexamplewithunittest/TutorialsExampleWithUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.tutorialsexamplewithunittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialsExampleWithUnitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
