5049a4a4d9a139222a25fd3b888992a55113af3b
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-14 20:42:10 +0530

    Two Sum - Find Pairs in an Array

--- problems-on-hashmap/src/main/java/com/hemanth/problemsonhashmap/ProblemsOnHashmapApplication.java
+++ problems-on-hashmap/src/main/java/com/hemanth/problemsonhashmap/ProblemsOnHashmapApplication.java
@@ -3,6 +3,9 @@ package com.hemanth.problemsonhashmap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class ProblemsOnHashmapApplication {
 
@@ -10,9 +13,25 @@ public class ProblemsOnHashmapApplication {
         SpringApplication.run(ProblemsOnHashmapApplication.class, args);
 
 
-
-
     }
+
+    /* 1. Two Sum - Find Pairs in an Array:
+     *   Problem: Given an array of integers, find two numbers such that they add up to a specific target.
+     * */
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
 
 
 
