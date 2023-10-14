13847a800460e38d534c7951f1ec201cdacfcdb8
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-14 20:46:06 +0530

    Subarray Sum Equals K

--- problems-on-hashmap/src/main/java/com/hemanth/problemsonhashmap/ProblemsOnHashmapApplication.java
+++ problems-on-hashmap/src/main/java/com/hemanth/problemsonhashmap/ProblemsOnHashmapApplication.java
@@ -72,6 +72,21 @@ public class ProblemsOnHashmapApplication {
     * Problem: Given an array of integers, find the total number of subarrays whose sum equals k.
     * */
 
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        Map<Integer, Integer> sumMap = new HashMap<>();
+        sumMap.put(0, 1);
+        for (int num : nums) {
+            sum += num;
+            if (sumMap.containsKey(sum - k)) {
+                count += sumMap.get(sum - k);
+            }
+            sumMap.put(sum, sumMap.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
+
 
 
 
