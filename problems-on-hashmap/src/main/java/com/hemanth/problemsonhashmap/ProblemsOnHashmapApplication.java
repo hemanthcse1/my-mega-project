69f25be72d368ecc474e281d35f5071309a724ee
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-14 20:40:37 +0530

    Hashmap problem solving

---
+++ problems-on-hashmap/src/main/java/com/hemanth/problemsonhashmap/ProblemsOnHashmapApplication.java
@@ -0,0 +1,19 @@
+package com.hemanth.problemsonhashmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemsOnHashmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProblemsOnHashmapApplication.class, args);
+
+
+
+
+    }
+
+
+
+}
---
+++ problems-on-hashmap/src/test/java/com/hemanth/problemsonhashmap/ProblemsOnHashmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.problemsonhashmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemsOnHashmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
