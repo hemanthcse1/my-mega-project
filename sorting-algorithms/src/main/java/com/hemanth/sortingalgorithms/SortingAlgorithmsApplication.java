bda43442cde1a588be8025924f112cef94ab91d9
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-15 20:54:53 +0530

    sorting algorithms initial commit

---
+++ sorting-algorithms/src/main/java/com/hemanth/sortingalgorithms/SortingAlgorithmsApplication.java
@@ -0,0 +1,14 @@
+package com.hemanth.sortingalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingAlgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SortingAlgorithmsApplication.class, args);
+        System.out.println("Sorting algorithms");
+    }
+
+}
