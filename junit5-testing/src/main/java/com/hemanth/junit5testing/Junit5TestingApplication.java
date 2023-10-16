28a63c8b99856bcbfc6719fd0a24ed18f90749ed
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-16 11:10:41 +0530

    Junit% Testing practice

---
+++ junit5-testing/src/main/java/com/hemanth/junit5testing/Junit5TestingApplication.java
@@ -0,0 +1,13 @@
+package com.hemanth.junit5testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5TestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Junit5TestingApplication.class, args);
+    }
+
+}
