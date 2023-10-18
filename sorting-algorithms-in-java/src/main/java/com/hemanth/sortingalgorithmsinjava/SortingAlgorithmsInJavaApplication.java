26c9a09576655b0343a80c9ad3490d3335cf85a5
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-18 12:42:08 +0530

    sorting algorithms using java

---
+++ sorting-algorithms-in-java/src/main/java/com/hemanth/sortingalgorithmsinjava/SortingAlgorithmsInJavaApplication.java
@@ -0,0 +1,13 @@
+package com.hemanth.sortingalgorithmsinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingAlgorithmsInJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SortingAlgorithmsInJavaApplication.class, args);
+    }
+
+}
