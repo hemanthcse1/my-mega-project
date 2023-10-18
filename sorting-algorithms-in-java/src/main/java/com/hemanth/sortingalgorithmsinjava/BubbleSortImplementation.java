47a58de22fbc0b0def2394713f45c38626bc639a
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-18 12:59:30 +0530

    Bubble sort

---
+++ sorting-algorithms-in-java/src/main/java/com/hemanth/sortingalgorithmsinjava/BubbleSortImplementation.java
@@ -0,0 +1,34 @@
+package com.hemanth.sortingalgorithmsinjava;
+
+import java.util.Arrays;
+
+public class BubbleSortImplementation {
+
+    public static void main(String[] args) {
+
+        int[] arr = {20, 35, -15, 7, 55, 1, -22};
+
+        for (int lastUnsortedIndex = arr.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+}
