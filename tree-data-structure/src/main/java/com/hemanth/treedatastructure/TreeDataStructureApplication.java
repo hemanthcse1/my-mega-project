b2cb052705206099950e69a82a53f33947f4ce57
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-19 12:44:17 +0530

    tree data structure " initial commit"

---
+++ tree-data-structure/src/main/java/com/hemanth/treedatastructure/TreeDataStructureApplication.java
@@ -0,0 +1,13 @@
+package com.hemanth.treedatastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeDataStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreeDataStructureApplication.class, args);
+    }
+
+}
