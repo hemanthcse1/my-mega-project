c68769f04c2ae0ebc17dca57876a01b997c942a2
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-20 11:32:52 +0530

    Design patterns using java

---
+++ design-patterns-java/src/main/java/com/hemanth/designpatternsjava/DesignPatternsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.hemanth.designpatternsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsJavaApplication.class, args);
+    }
+
+}
