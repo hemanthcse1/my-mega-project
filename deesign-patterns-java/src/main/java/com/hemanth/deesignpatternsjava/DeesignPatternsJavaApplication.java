addad7127c1d1268d70e3dcc264e7a4f2ab8c5e1
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-20 11:26:39 +0530

    Design patterns using java

---
+++ deesign-patterns-java/src/main/java/com/hemanth/deesignpatternsjava/DeesignPatternsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.hemanth.deesignpatternsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeesignPatternsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeesignPatternsJavaApplication.class, args);
+    }
+
+}
---
+++ deesign-patterns-java/src/test/java/com/hemanth/deesignpatternsjava/DeesignPatternsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemanth.deesignpatternsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeesignPatternsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
