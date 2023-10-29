24f9cacd628a923f5e2fb751a54492b88601bcb4
hemanthcse1 <hemanthcse1@gmail.com>
2023-10-29 15:10:17 +0530

    Graphs datastructures

---
+++ graphs-ds/src/main/java/com/hemanth/graphsds/GraphsDsApplication.java
@@ -0,0 +1,13 @@
+package com.hemanth.graphsds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphsDsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphsDsApplication.class, args);
+    }
+
+}
