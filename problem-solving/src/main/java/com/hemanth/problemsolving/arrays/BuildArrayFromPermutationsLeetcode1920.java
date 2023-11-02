6645d7f279ca630490af97ba3d62f60c33a31568
hemanthcse1 <hemanthcse1@gmail.com>
2023-11-02 08:20:02 +0530

    Build array from permutation leetcode-1920

---
+++ problem-solving/src/main/java/com/hemanth/problemsolving/arrays/BuildArrayFromPermutationsLeetcode1920.java
@@ -0,0 +1,26 @@
+package com.hemanth.problemsolving.arrays;
+
+import java.util.Arrays;
+
+public class BuildArrayFromPermutationsLeetcode1920 {
+
+
+    public static void main(String[] args) {
+
+
+        int[] nums = {0, 2, 1, 5, 3, 4};
+
+        System.out.println(Arrays.toString(buildArray(nums)));
+    }
+
+    public static int[] buildArray(int[] nums) {
+
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = nums[nums[i]];
+        }
+
+        return result;
+    }
+}
