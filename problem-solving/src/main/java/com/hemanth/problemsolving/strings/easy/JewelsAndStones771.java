ba2c77e3ae58385c469e2d5d76ff2ee46c08116d
hemanthcse1 <hemanthcse1@gmail.com>
2023-11-09 21:10:04 +0530

    Jewels and stones Leetcode-771

--- problem-solving/src/main/java/com/hemanth/problemsolving/strings/easy/JewelsAndStones771.java
+++ problem-solving/src/main/java/com/hemanth/problemsolving/strings/easy/JewelsAndStones771.java
@@ -1,4 +1,29 @@
 package com.hemanth.problemsolving.strings.easy;
 
 public class JewelsAndStones771 {
+
+    public static void main(String[] args) {
+
+        String jewels = "z";
+        String stones = "ZZ";
+
+
+        System.out.println(numJewelsInStones(jewels,stones));
+
+    }
+
+    public static int numJewelsInStones(String jewels, String stones){
+
+        int count = 0;
+        for (char c: stones.toCharArray()){
+
+            for (char d: jewels.toCharArray()){
+                if (c == d){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
 }
