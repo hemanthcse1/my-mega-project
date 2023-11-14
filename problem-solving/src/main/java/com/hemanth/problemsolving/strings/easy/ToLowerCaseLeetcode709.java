b6b8979856120805cda0ea3f51e28a636d0a9e4a
hemanthcse1 <hemanthcse1@gmail.com>
2023-11-14 18:56:32 +0530

    To Lower case Leetcode-709

---
+++ problem-solving/src/main/java/com/hemanth/problemsolving/strings/easy/ToLowerCaseLeetcode709.java
@@ -0,0 +1,17 @@
+package com.hemanth.problemsolving.strings.easy;
+
+public class ToLowerCaseLeetcode709 {
+
+    public static void main(String[] args) {
+
+        String s= "Hellow";
+
+        System.out.println(toLowerCase(s));
+    }
+
+
+    public static String toLowerCase(String s){
+
+        return s.toLowerCase();
+    }
+}
