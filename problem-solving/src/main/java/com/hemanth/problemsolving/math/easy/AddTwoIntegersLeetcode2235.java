2fe4c870d9dc49070cbc64a1869b0d8e7e6f88b3
hemanthcse1 <hemanthcse1@gmail.com>
2023-11-15 20:14:31 +0530

    Add Two integers leetcode-2235

---
+++ problem-solving/src/main/java/com/hemanth/problemsolving/math/easy/AddTwoIntegersLeetcode2235.java
@@ -0,0 +1,17 @@
+package com.hemanth.problemsolving.math.easy;
+
+public class AddTwoIntegersLeetcode2235 {
+
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 5;
+
+
+        System.out.println(sum(num1,num2));
+
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
