5ddc0eaaee5bdf5ed5cbe52fe5f2db36bd1dab57
hemanthcse1 <hemanthcse1@gmail.com>
2023-11-22 20:37:53 +0530

    sum of unique elements leetcode-1745

--- problem-solving/src/main/java/com/hemanth/problemsolving/hashtable/SumOfUniqueElements1745.java
+++ problem-solving/src/main/java/com/hemanth/problemsolving/hashtable/SumOfUniqueElements1745.java
@@ -15,6 +15,33 @@ public class SumOfUniqueElements1745 {
 
     }
 
+    /*public static int sumOfUniqueElements(int[] nums){
+        int sum = 0;
+
+        Map<Integer,Integer> uniqueElements = new HashMap<>();
+
+        for (int i = 0; i< nums.length; i++){
+
+            if (uniqueElements.containsKey(nums[i])){
+                int count = uniqueElements.get(nums[i]);
+                count++;
+                uniqueElements.put(nums[i],count);
+            }else {
+                uniqueElements.put(nums[i],0);
+            }
+        }
+
+        for (Integer key : uniqueElements.keySet()) {
+            if (uniqueElements.get(key) == 0){
+                sum+=key;
+            }
+        }
+
+        return sum;
+
+    }*/
+
+
     public static int sumOfUniqueElements(int[] nums){
         int sum = 0;
 
